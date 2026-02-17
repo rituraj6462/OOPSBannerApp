55419151dfdbbb4e2db931fac8ff6d4ca65c924f
rituraj6462 <rr6462@srmist.edu.in>
2026-02-17 13:33:46 +0530

    Coded UC1 to Print OOPS to Console

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // Your code starts here
+
+        System.out.println("Hello, World!");
+
+        // Your code ends here
+    }
+}
